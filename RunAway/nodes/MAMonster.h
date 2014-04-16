9c39060a3cc6ee64d9c5036f30e4fc259ee6b125
Marlon Andrade <marlonmandrade@gmail.com>
2014-04-15 23:31:23 -0300

    adding monster and renaming player node

---
+++ RunAway/nodes/MAMonster.h
@@ -0,0 +1,13 @@
+//
+//  MAMonster.h
+//  RunAway
+//
+//  Created by Marlon Andrade on 15/04/14.
+//  Copyright (c) 2014 MarlonAndrade. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MAMonster : SKSpriteNode
+
+@end
---
+++ RunAway/nodes/MAMonster.m
@@ -0,0 +1,13 @@
+//
+//  MAMonster.m
+//  RunAway
+//
+//  Created by Marlon Andrade on 15/04/14.
+//  Copyright (c) 2014 MarlonAndrade. All rights reserved.
+//
+
+#import "MAMonster.h"
+
+@implementation MAMonster
+
+@end
